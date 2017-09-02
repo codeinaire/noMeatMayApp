d5f4071337523f8e975e80193fd8bdd676d4100e
codeinaire <johncodeinaire@gmail.com>
2017-09-02 15:52:46 +1000

    Add shared button component

--- design/designNotes.md
+++ design/designNotes.md
@@ -1,3 +1,5 @@
 # Design Notes
 
 This is a file for any design notes that I may have for this project.
+
+https://www.fullstackreact.com/p/using-presentational-and-container-components-with-redux/
--- nmm-react-front/src/sharedDumbComponents/SharedButton.js
+++ nmm-react-front/src/sharedDumbComponents/SharedButton.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  border: 1px solid #e1a904;
+  border-radius: .28571429rem;
+  min-height: 1em;
+  color: #fff;
+  background-color: #fbbd08;
+  font-size: 1rem;
+  padding: .78571429em 1.5em .78571429em;
+  font-family: 'Lato','Helvetica Neue', 'Arial', 'Helvetica', sans-serif;
+
+  &:hover {
+    background-color: #e1a904;
+  }
+`;
 
 const SharedButton = (props) => (
   <div>
-    <button>{props.title}</button>
+    <Button>
+      {props.title}
+    </Button>
   </div>
 )
 
 SharedButton.propTypes = {
-  test: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
 
 export default SharedButton;
